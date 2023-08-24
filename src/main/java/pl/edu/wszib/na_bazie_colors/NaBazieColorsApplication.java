15bb55aabb8b83813306e7584db6a99285e22e48
MarcinLes7 <Gitm@rtino7>
2023-08-24 15:15:00 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/na_bazie_colors/NaBazieColorsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.na_bazie_colors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaBazieColorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaBazieColorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/na_bazie_colors/NaBazieColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.na_bazie_colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaBazieColorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
